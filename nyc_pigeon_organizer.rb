5a58d6d9cd2d45eb28e18cd00f5780178be3cce6
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-07 16:02:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value| 
     puts "this is a key: #{key}. "
     puts "this is a value: #{value}"
-    value.each do |attribute|
+    value.each do |attribute, list|
       puts "this #{key} is #{attribute}"
     end
   end
